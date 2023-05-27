58371159892d01bea4a35e1ad096362e97626144
Noah <noah.endale@gmail.com>
2023-05-27 15:54:18 -0400

    Add favourite icons, use localStorage to persist favourites

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from '@emotion/styled'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 interface ProductInterface {
   category: string;
@@ -23,7 +25,7 @@ const ProductContainer = styled.div`
   margin: 1.5rem 0.5rem;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: flex-start;
   padding: 1rem;
   border: 1px solid black;
   border-radius: 10px;
@@ -53,35 +55,103 @@ const ImageContainer = styled.div`
   }
 `
 
-const Product = ({productList}: ProductListInterface) => (
-  <>
-    {productList.map((product: ProductInterface) => {
-      const {title, image, price, id} = product
+const StyledButton = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
 
-      // Different number formats coming from API so I'm formating everything here
-      const CADollar = new Intl.NumberFormat('en-CA', {
-        style: 'currency',
-        currency: 'CAD',
-      })
+  &.border {
+    border: 1px solid black;
+  }
 
-      return (
-        <ProductContainer
-          key={id}
-          className='app__product-container'
-          tabIndex={0}
-        >
-          <ImageContainer>
-            <img
-              src={image}
-              alt={title}
-            />
-          </ImageContainer>
-          <h4>{title}</h4>
-          <p>C{CADollar.format(price)}</p>
-        </ProductContainer>
-      )}
-    )}
-  </>
-)
+  &.favourite {
+    text-align: left;
+    width: max-content;
+    transition: color 0.2s;
+    color: #e92d2d;
+  }
+`
+
+const DescriptionParagraph = styled.p`
+  transition: all 0.2s;
+  opacity: 0;
+  max-height: 0;
+
+  &.visible {
+    opacity: 1;
+    max-height: 10rem;
+  }
+`
+
+const Product = ({ productList }: ProductListInterface) => {
+  const [showDescriptions, setShowDescriptions] = useState(
+    productList.map(() => false)
+  );
+
+  const [favourites, setFavourites] = useState(() => {
+    // check if anything in localstorage before initializing state
+    const storedFavourites = localStorage.getItem('favourites')
+    if (storedFavourites !== null) return JSON.parse(storedFavourites)
+  })
+
+  useEffect(() => {
+    //update localstorage whenever favourites changes
+    localStorage.setItem('favourites', JSON.stringify(favourites))
+  }, [favourites])
+
+  const toggleDescription = (index: number) => {
+    setShowDescriptions((prevState) => {
+      // make a copy of current state and toggle the state of 
+      // the passed in index
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
+  const toggleFavourite = (id: number) => {
+    setFavourites((prevState: number[]) => {
+      const newState = [...prevState];
+      const idIndex = newState.indexOf(id)
+
+      idIndex === -1 ? newState.push(id) : newState.splice(idIndex, 1)
+      return newState;
+    })
+  }
+
+  return (
+    <>
+      {productList.map((product: ProductInterface, index: number) => {
+        const { title, image, price, id, description } = product;
+
+        const CADollar = new Intl.NumberFormat('en-CA', {
+          style: 'currency',
+          currency: 'CAD',
+        });
+
+        return (
+          <ProductContainer
+            key={id}
+            className="app__product-container"
+            tabIndex={0}
+          >
+            <StyledButton className='favourite' onClick={() => toggleFavourite(id)}>
+              {favourites.indexOf(id) === -1 ? <FavoriteBorderIcon /> : <FavoriteIcon/>}
+            </StyledButton>
+            <ImageContainer>
+              <img src={image} alt={title} />
+            </ImageContainer>
+            <h4>{title}</h4>
+            <StyledButton onClick={() => toggleDescription(index)} className='border'>
+              {showDescriptions[index] ? 'Hide' : 'Show More'}
+            </StyledButton>
+            <DescriptionParagraph className={showDescriptions[index] ? 'visible' : ''}>{description}</DescriptionParagraph>
+            <p>{CADollar.format(price)}</p>
+          </ProductContainer>
+        );
+      })}
+    </>
+  );
+};
 
 export default Product
